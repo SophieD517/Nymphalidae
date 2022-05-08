9b3a02900a24620fe1b25fe5e9591e20985f5b94
SophieD517 <93719167+SophieD517@users.noreply.github.com>
2022-05-08 15:33:58 -0400

    Update Plexippus.py

--- Danaus/Plexippus.py
+++ Danaus/Plexippus.py
@@ -1,6 +1,6 @@
 import torch
 
-class Plexippus(torch.nn.Module):
+class ConvMulti(torch.nn.Module):
   def __init__(self, latent_dim, ae_layer1, ae_layer2, branch_layer, num_branches):
     super().__init__()
     self.enc_conv1 = torch.nn.Conv1d(in_channels=1, out_channels=ae_layer1, kernel_size=200)
@@ -43,6 +43,55 @@ class Plexippus(torch.nn.Module):
     except:
       self.enc_conv1 = torch.nn.Conv1d(in_channels=1, out_channels=ae_layer1, kernel_size=x.shape[1])
       self.dec_lin1 = torch.nn.Linear(ae_layer1, x.shape[1])
+    preds = []
+    for branch in self.branches:
+      preds.append(branch(ls))
+    return ls, self.decoder(ls), preds
+
+  
+  
+  class FCMulti(torch.nn.Module):
+  def __init__(self, latent_dim, ae_layer, branch_layer, num_branches):
+    super().__init__()
+    self.latent_dim, self.ae_layer, self.branch_layer = latent_dim, ae_layer, branch_layer
+    self.num_branches = num_branches
+    self.branches = []
+    
+    for i in range(self.num_branches):
+      self.branches.append(torch.nn.Sequential(
+        torch.nn.ReLU(),
+        torch.nn.Linear(latent_dim, branch_layer),
+        torch.nn.ReLU(),
+        torch.nn.Linear(branch_layer, 1)
+      ))
+
+    self.encoder = torch.nn.Sequential(
+      torch.nn.Linear(100, ae_layer),
+      torch.nn.ReLU(),
+      torch.nn.Linear(ae_layer, latent_dim)
+    )
+    self.decoder = torch.nn.Sequential( 
+      torch.nn.ReLU(),
+      torch.nn.Linear(latent_dim, ae_layer),
+      torch.nn.ReLU(),
+      torch.nn.Linear(ae_layer, 100)
+    )
+      
+  def forward(self, x):
+    try: 
+      ls = self.encoder(x)
+    except:
+      self.encoder = torch.nn.Sequential(
+        torch.nn.Linear(x.shape[1], self.ae_layer),
+        torch.nn.ReLU(),
+        torch.nn.Linear(self.ae_layer, self.latent_dim)
+      )
+      self.decoder = torch.nn.Sequential( 
+        torch.nn.ReLU(),
+        torch.nn.Linear(self.latent_dim, self.ae_layer),
+        torch.nn.ReLU(),
+        torch.nn.Linear(self.ae_layer, x.shape[1])
+      )
     preds = []
     for branch in self.branches:
       preds.append(branch(ls))
