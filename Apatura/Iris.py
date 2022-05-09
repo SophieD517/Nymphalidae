1125be9d637e90101c587a848217e6303eeed971
SophieD517 <93719167+SophieD517@users.noreply.github.com>
2022-05-09 16:26:42 -0400

    add some testing functions

--- Apatura/Iris.py
+++ Apatura/Iris.py
@@ -16,12 +16,13 @@ class WorkUnit:
       self.model = Plexippus.FCMulti(latent_dim, ae_layer, branch_layer, 1)
     elif model=='Conv':
       self.model = Plexippus.ConvMulti(latent_dim, ae_layer1, ae_layer2, branch_layer, 1)
-    self.learning_rate, self.num_branches = learning_rate, num_branches
+    self.learning_rate, self.num_branches, self.latent_dim = learning_rate, num_branches, latent_dim
     self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
     self.losses = []
 
   def train(self, x, branches, epochs, batch_size, weights='hi', graph_loss=True,save_ls=False): 
     self.batch_size = batch_size
+    self.num_branches = branches.shape[1]
     if weights=='hi':
       self.weights = np.full(shape=branches.shape[1], fill_value=1,dtype=int)
     assert branches.shape[1]=weights.shape[0], 'weights of wrong size'
@@ -35,7 +36,7 @@ class WorkUnit:
           for i in range(self.num_branches):
             loss.append(weights[i]*self.loss_func(preds[i].squeeze(), batch[i].squeeze())
         except Indexerror:
-          self.model.num_branches = branches.shape[1]
+          self.model.num_branches = self.num_branches
         loss.backward(retain_graph=True)
         self.optimizer.step()
         self.losses.append(loss.detach().numpy())
@@ -56,3 +57,25 @@ class WorkUnit:
 
   def save(self, file_path):
     torch.save(self.model.state_dict(), file_path)
+
+                        
+  def test(self, x, branches, file_path):
+    self.x_test, self.test_branches = x, branches
+    weights = torch.load(file_path)
+    self.model.load_state_dict(weights)
+    self.model.eval()
+    latent_space, recon, preds = self.ae_model(self.x_test)
+    self.test_ls, self.test_recon, self.preds = latent_space, recon, preds
+    self.loss = self.loss_func(reconstructed.squeeze(), batch[0].squeeze())
+      for i in range(self.num_branches):
+        self.loss.append(weights[i]*self.loss_func(preds[i].squeeze(), batch[i].squeeze())
+    
+  def graph_correlation(self, var, name=var, width=0.3):
+    plt.style.use('seaborn-whitegrid')
+    dt = self.test_branches[var]
+    pred = self.preds[var]
+    plt.plot(dt.mul(5).detach().numpy().reshape(-1,), dt.mul(5).detach().numpy().reshape(-1,), color='gray', linewidth=0.1)
+    plt.scatter(pred.mul(5).detach().numpy().reshape(-1,), dt.mul(5).detach().numpy().reshape(-1,), color='purple', s=width)
+    plt.xlabel('predicted')
+    plt.ylabel('actual')
+    plt.title(name + '-value')
